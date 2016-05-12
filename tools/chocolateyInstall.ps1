c1350f79e1b01baddcbee7933114e3269f482466
Andy Li <andy@onthewings.net>
2016-05-12 21:24:48 +0800

    4.02.1.20160113

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-$url        = 'http://www.ocamlpro.com/pub/ocpwin/ocpwin-builds/ocpwin32/20160113/ocpwin32-20160113-4.01.0+ocp1-msvc32.exe'
-$url64      = 'http://www.ocamlpro.com/pub/ocpwin/ocpwin-builds/ocpwin64/20160113/ocpwin64-20160113-4.01.0+ocp1-msvc64.exe'
+$url        = 'http://www.ocamlpro.com/pub/ocpwin/ocpwin-builds/ocpwin32/20160113/ocpwin32-20160113-4.02.1+ocp1-msvc32.exe'
+$url64      = 'http://www.ocamlpro.com/pub/ocpwin/ocpwin-builds/ocpwin64/20160113/ocpwin64-20160113-4.02.1+ocp1-msvc64.exe'
 
 $packageArgs = @{
   packageName   = 'ocpwin'
